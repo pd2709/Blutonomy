7693643920b33d348e29d23f6ebe582a9c590dd4
pd2709 <88146518+pd2709@users.noreply.github.com>
2022-05-06 01:48:50 +1000

    imu data subscriber draft

---
+++ subscribers/data_sub.cpp
@@ -0,0 +1,35 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+//include any other 
+
+void imuCallback(const std_msgs::String::ConstPtr& msg)     //need to input correct data type
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str()); //update ROS INFO
+}
+
+void imuDataCallback(const std_msgs::String::ConstPtr& msg)     //need to input correct data type
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str()); //update ROS INFO
+}
+
+void imuDatarawCallback(const std_msgs::String::ConstPtr& msg)     //need to input correct data type
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str()); //update ROS INFO
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "mavros_imu_subscriber");
+  ros::NodeHandle n;
+  ros::Subscriber subImu = n.subscribe("/mavros/imu", 10, imuCallBack);
+  ros::Subscriber subImuData = n.subscribe("/mavros/imu/data", 10, imuDataCallback);
+  ros::Subscriber subImuDataRaw = n.subscribe("/mavros/imu/data_raw", 10, imuDatarawCallback);
+  ros::spin();
+  return 0;
+}
+
+
+//sources used:
+//https://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28c%2B%2B%29
+// https://www.stereolabs.com/docs/ros/sensor-data/
+//https://github.com/stereolabs/zed-ros-examples/blob/master/tutorials/zed_sensors_sub_tutorial/src/zed_sensors_sub_tutorial.cpp
