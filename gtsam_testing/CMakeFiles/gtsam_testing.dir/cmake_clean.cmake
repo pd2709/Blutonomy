751833a57e0765060699f9410255f0ddafe4cda2
Melody Hood <85168871+melhood@users.noreply.github.com>
2022-06-08 17:19:30 +1000

    Add files via upload

---
+++ gtsam_testing/CMakeFiles/gtsam_testing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtsam_testing.dir/3DLandmarkTrial.cpp.o"
+  "gtsam_testing.pdb"
+  "gtsam_testing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtsam_testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
